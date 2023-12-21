7405ad4988debf389b64a8494bc6888c1e38d8c0
blackjack-ttp <phongtt120298@outlook.com>
2023-12-21 16:50:08 +0700

    Bai1

--- bai_1.py
+++ bai_1.py
@@ -12,9 +12,6 @@ image_path = os.path.join(current_dir, "image", "1.png")
 wanted_text = tk.Label(app, text="WANTED", font=("Times New Roman", 50))
 wanted_text.pack()
 
-# Mở hình ảnh bằng Pillow và chuyển đổi thành ImageTk.PhotoImage và sau đó đưa cho tkinter hiển thị lên 
-# # Tại vì nếu k có bước nảy thì thư viện tkinter nó chỉ đọc được hình ảnh với đúng định dạng mà nó hỗ trợ còn nếu có 
-# # # bước này thì ảnh nào cũng chơi kể cả ảnh 18+
 cat_image_pil = Image.open(image_path)
 cat_image = ImageTk.PhotoImage(cat_image_pil)
 
