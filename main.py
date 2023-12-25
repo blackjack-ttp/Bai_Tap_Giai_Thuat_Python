ed95f86a6618e82ef4de7256cfc19a575054f4ee
blackjack-ttp <phongtt120298@outlook.com>
2023-12-25 11:36:42 +0700

    dự án Tic-Tac-Toe

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import bai_1
-import bai_2
-import bai_3
+# import bai_1
+# import bai_2
+# import bai_3
+import bai_4
 
 # dự án Wanted Poster
 # bai_1
 # dự án SPY name choose
 # bai_2
 # dự án Meme
-bai_3
+# bai_3
+# dự án Tic-Tac-Toe
+bai_4
